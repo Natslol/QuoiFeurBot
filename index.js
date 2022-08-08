2ec91d0407c1e927d2e29a76763043ac38892a40
Ludo <109055653+Ludo1337@users.noreply.github.com>
2022-08-08 03:11:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const config = require('./config.json')
 
 client.on('ready', () => {
 	console.log(`Quoi ? ${client.user.tag}!`)
-	client.user.setActivity(`Ludo`, { type: 'LISTENING'})
+	client.user.setActivity(`Ludo`, { type: "LISTENING", url: "https://github.com/Ludo1337/QuoiFeurBot/"})
 });
 
 client.on('message', message => {
-	var char = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","/","\\","!","`",""]
+var char = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","/","\\","!","`",""]
     for(a in char) {
         if (message.content.endsWith(char[a])) {
             var p = message.content;
@@ -20,8 +20,8 @@ client.on('message', message => {
             quoi.forEach(i => {
                 var split = p.split(' ');
                 if (split[split.length - 1].includes(i)) {
-                    rand = ['Feur', 'Feur comme le metier de ta soeur', 'Feur tête de chou fleur', 'Feur à domicile', 'Feur vas te coiffer tu me fais peur', 'Feur tu veux la coupe à ton chauffeur ?', 'Feur tu veux monter sur mon tracteur ?', "Feur j'ai plus de rimes en eur", "Feur surement"];
-                    console.log(`${message.author.tag} (${message.author.id}) c'est pris un petit feur dans ${message.guild} (${message.guild.id})`)
+                    rand = ["Feur", "Feur comme le metier de ta soeur", "Feur tête de chou fleur", "Feur à domicile", "Feur vas te coiffer tu me fais peur", "Feur tu veux la coupe à ton chauffeur ?", "Feur tu veux monter sur mon tracteur ?", "Feur j'ai plus de rimes en eur", "Feur surement"];
+                    console.log(`${message.author.tag} (${message.author.id}) c'est pris un peti" feur dans ${message.guild} (${message.guild.id})`)
                     message.reply(rand[Math.floor(Math.random()*rand.length)]);
                 }
             })
